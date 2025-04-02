794afa5bc6a62bda5f4b1eaa068eb5c6e2b7dcd7
Cristan Sanchez <diego.cristan123@gmail.com>
2025-04-02 11:14:30 -0600

    Implementacion de los metodos en Administrador: modNombre, modEdad, modTelefono, modDireccion. Implementacion de agregarInvitado y agregarUsuario pero aun vacios

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,8 +1,31 @@
 public class Administrador extends Persona implements Configuracion { //Administrador es subclase de Persona e implementa la interface Configuracion
 
-    public void modificarNombre(){
+    public Administrador(String nombre, String apellidoP, String apellidoM, String CURP, String direccion, int edad){
+        super(nombre, apellidoP, apellidoM, CURP, direccion, edad);
+    }
+
+    @Override
+    public void modificarNombre(String nombre){ //Metodo implementado de configuracion para modificar el nombre
+        this.nombre = nombre;
+    }
+    @Override
+    public void modificarEdad(int edad){ //Metodo implementado de configuracion para modificar la edad
+        this.edad = edad;
+    }
+    @Override
+    public void modificarTelefono(int telefono){ //Metodo implementado de configuracion para modificar el telefono
+        this.telefono = telefono;
+    }
+    @Override
+    public void modificarDireccion(String direccion){ //Metodo implementado de configuracion para modificar la direccion
+        super.direccion = direccion;
+    }
+    @Override
+    public void agregarInvitado(){ //Metodo abstracto heredado de Persona
         
     }
-    public void modificarTelefono(){}
-    public void modificarDireccion(){}
+    @Override
+    public void agregarUsuario(){ //Metodo abstracto heredado de Persona
+
+    }
 }
