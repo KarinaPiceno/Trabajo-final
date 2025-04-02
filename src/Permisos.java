21f63ed4f14264e035723adeb8379e7b61639056
Karina Piceno <karina.kipc@gmail.com>
2025-04-02 00:51:23 -0600

    correcciones y notas de enumeración permisos

--- src/Permisos.java
+++ src/Permisos.java
@@ -1,4 +1,7 @@
 public enum Permisos {
+//que estabas inteentando hacer?? no entiendo tu intención en este código
+//lo unico que tienes que hacer es una enumeracion con los dos peermisos que te pasé, los valores de estos se van a usar para encontrar
+// el índice en un vector donde se van a guardar los permisos que se les van a asignar a usuarios que no son admin. No te compliques la existencia.
 
     // agregar invitado??
     // validar por nombre, asignandole un id?
