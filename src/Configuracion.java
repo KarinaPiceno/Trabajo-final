249f82d5fb1cfaf6fc3aae7c4fda87da9117ac56
Cristan Sanchez <diego.cristan123@gmail.com>
2025-04-02 10:25:36 -0600

    Creacion de los metodos de la interfaz Configuracion

--- src/Configuracion.java
+++ src/Configuracion.java
@@ -1,3 +1,6 @@
 public interface Configuracion {
-
+    public void modificarNombre();
+    public void modificarEdad();
+    public void modificarTelefono();
+    public void modificarDireccion();
 }
