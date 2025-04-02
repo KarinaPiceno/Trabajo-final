b6ecec7bed0863a89eac4ac4fa9f4245e8313c0a
Kahleb <kahlebalejandro.ramirezrobles@umg.edu.mx>
2025-04-02 08:17:01 -0600

    Segunda rev
    Cambios en en ingreso, falta enum

--- src/Ingresos.java
+++ src/Ingresos.java
@@ -1,70 +1,52 @@
+import java.time.*;
+
+
 public class Ingresos{
-/*extends Persona? o interface?
-    ninguno, es una clase independiente de tipo static
-    tienes dos opciones, o aqui arriva le pones static, o en cada atributo y método le pones static 
-*/ 
+
    
-    private static String log[][];  //por el momento siempre se va a hacer un vector, no una matriz, y va a almacenar los diferentes logs
-
-    //static?
-    private static final int id = 652; //por que estas creando una id? vas a usar la id de la clase de admin y usuario
-
-    /*     |super() para persona?| nop, la clase es independiente a persona
-    public static int getId() {
-        return id;
-
-    }
-    */
-
-    //que hace entrada?
-    //es un string que contiene el nombre del invitado o el id del invitado?
+//Vector estatico
+    private static String log[];  
     public String entrada; 
-      
     
-        //en el set entrada vas a agregar al vector el string resultante del metodo toString()
-    public void setEntrada(String entrada) {
-        this.entrada = entrada;
-    }
-        // en el get entrada vas a pedir un int para el indice y vas a devolver el string que se encuentre en ese índice
-    public String getEntrada() {
-        return entrada;
+      
+
+ //Vector estatico para guardar los logs de los ingresos
+//id para el log de ingresos para persona 
+    public static String getEntrada(int indice) {
+        return log[indice];
     }
     
 
-    //super() para persona? nop
+    
     public static int getId() {
         return id;  
     }
 
-    
-    //Verifica si el id es correcto, si no lo es retorna false, si lo es retorna true
-
-    //supongo que el id se validara en el servidor
-         // si y no, por el momento se va a validar aquí, lo que va a suceder es que tienes que pedir un int que es el id, y vas a pedir 
-         //2 string, 1 para key1 y otro para key2. dentro del método vas a comparar el key1 de persona con el key 1 que recibiste y el key2 
-        //de persona con el key2 que recibiste. si ambos coinciden entonces vas a llamar al setEntrada y ademas va a regresar un true.
-    // Por ahora solo se verifica si el id es el que esta inicializado
-    public final static boolean verificarId(int id) {
+//compara key1 con key2, si son iguales regersa true con la fecha y hora actual, si no son iguales regresa false
+    public final static boolean verificarId(Persona p1, String key1, String key2) {
         
-        if (id == 652) {
-            System.out.println("ID valido");
+        if (p1.key1 == key1 && p1.key2 == key2) {
+            
+            setEntrada(p1.getNombreCompleto() + " - " + java.time.LocalDateTime.now());
             return true;
         } else {
-            System.out.println("ID invalido");
+
             return false;
         }
     }
 
 
 
-        //en el override de string va a ser el formato Hora + fecha + nombre, el nombre lo puedes sacar del método getNombreCompleto() de la clase persona
     @Override
-    public String toString() {
-        return "Ingresos{" +
-                "entrada='" + entrada + '\'' +
-                ", id=" + id +
-                '}';
+    public String toString () {
+        LocalDateTime fechaHoraActual = LocalDateTime.now();
+        return fechaHoraActual.toString() + " - " + Persona.getNombreCompleto();
     }
+///entrada de datos, se guarda el log de la persona que ingreso al sistema
+    public static void setEntrada(int indice, String log) {
+        Ingresos.log[indice] = log;
+    }
+
 
 
 
--- src/Permisos.java
+++ src/Permisos.java
@@ -3,44 +3,6 @@ public enum Permisos {
 //lo unico que tienes que hacer es una enumeracion con los dos peermisos que te pasé, los valores de estos se van a usar para encontrar
 // el índice en un vector donde se van a guardar los permisos que se les van a asignar a usuarios que no son admin. No te compliques la existencia.
 
-    // agregar invitado??
-    // validar por nombre, asignandole un id?
 
-    ACCESO_ADMINISTRADOR("Acceso a administrador"),
-    ACCESO_INVITADO("Acceso a invitado"),
-    ACCESO_USUARIO("Acceso al usuario"),;
-
-    private final String descripcion;
-
-    Permisos(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
- //en otra clase
-    public permisos inv;
-    public permisos usuario;
-    public permisos admin;
-    
-
-//en otra calse 
-    // permisos de invitado
-    public invitado() {
-        invitado = Permisos.invitado;
-        if (invitado == Permisos.admin) {
-            System.out.println("Acceso a invitado");
-        } else {
-            System.out.println("No tiene acceso a invitado");
-        }
-
-    }
-
-
-
-
-    //permisos de administrador
-    modificar_Datos 
+   
 }
