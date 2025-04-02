ec82d5872a3716abc1f621309c1ade88d165568a
Kahleb <kahlebalejandro.ramirezrobles@umg.edu.mx>
2025-04-02 08:21:28 -0600

    Segunda rev 1.1
    enum

--- src/Permisos.java
+++ src/Permisos.java
@@ -1,8 +1,8 @@
 public enum Permisos {
-//que estabas inteentando hacer?? no entiendo tu intención en este código
-//lo unico que tienes que hacer es una enumeracion con los dos peermisos que te pasé, los valores de estos se van a usar para encontrar
-// el índice en un vector donde se van a guardar los permisos que se les van a asignar a usuarios que no son admin. No te compliques la existencia.
 
 
+//por revisar, posiblemente se use en otras clases, 
+    AGREGAR_INVITADO,
+    MODIFICAR_DATOS
    
 }
