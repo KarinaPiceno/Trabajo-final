e7d8bcfe06c6c10abd1cbaba7898403f1cb1e88e
Kahleb <kahlebalejandro.ramirezrobles@umg.edu.mx>
2025-04-02 00:07:25 -0600

    Primera rev
    Posibles cambios y sugerencias

--- src/Ingresos.java
+++ src/Ingresos.java
@@ -1,3 +1,74 @@
-public class Ingresos {
+public class Ingresos{
+/*extends Persona? o interface?*/
+   
+    private static String log[][];
 
+    //static?
+    private static final int id = 652;
+
+    /*     |super() para persona?|
+    public static int getId() {
+        return id;
+    }
+    */
+
+    //que hace entrada?
+    //es un string que contiene el nombre del invitado o el id del invitado?
+    public String entrada; 
+      
+    
+
+    public void setEntrada(String entrada) {
+        this.entrada = entrada;
+    }
+    public String getEntrada() {
+        return entrada;
+    }
+    
+
+    //super() para persona?
+    public static int getId() {
+        return id;  
+    }
+
+
+    //Verifica si el id es correcto, si no lo es retorna false, si lo es retorna true
+
+    //supongo que el id se validara en el servidor
+    // Por ahora solo se verifica si el id es el que esta inicializado
+    public final static boolean verificarId(int id) {
+        
+        if (id == 652) {
+            System.out.println("ID valido");
+            return true;
+        } else {
+            System.out.println("ID invalido");
+            return false;
+        }
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Ingresos{" +
+                "entrada='" + entrada + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+
+
+    
 }
+
+
+
+     
+
+
+
+    
+    
+
--- src/Permisos.java
+++ src/Permisos.java
@@ -1,3 +1,43 @@
 public enum Permisos {
 
+    // agregar invitado??
+    // validar por nombre, asignandole un id?
+
+    ACCESO_ADMINISTRADOR("Acceso a administrador"),
+    ACCESO_INVITADO("Acceso a invitado"),
+    ACCESO_USUARIO("Acceso al usuario"),;
+
+    private final String descripcion;
+
+    Permisos(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+ //en otra clase
+    public permisos inv;
+    public permisos usuario;
+    public permisos admin;
+    
+
+//en otra calse 
+    // permisos de invitado
+    public invitado() {
+        invitado = Permisos.invitado;
+        if (invitado == Permisos.admin) {
+            System.out.println("Acceso a invitado");
+        } else {
+            System.out.println("No tiene acceso a invitado");
+        }
+
+    }
+
+
+
+
+    //permisos de administrador
+    modificar_Datos 
 }
