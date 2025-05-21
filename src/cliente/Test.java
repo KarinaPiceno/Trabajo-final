ef341b9da3f4869dc04252196341475640d10915
Cristan Sanchez <diego.cristan123@gmail.com>
2025-05-21 10:28:04 -0600

    Implementacion de Invitado

--- src/cliente/Test.java
+++ src/cliente/Test.java
@@ -3,6 +3,6 @@ package cliente;
 public class Test {
     public static void main(String args[])
     {
-        ServerCliente client = new ServerCliente("172.16.16.147", 5000);
+        ServerCliente client = new ServerCliente("172.16.14.70", 5000);
     }
 }
